281e00b5550d12d6172694c5bb5892d9c5d1352d
alysbrett <alysbrett@users.noreply.github.com>
2015-03-27 14:59:12 +0000

    Tidying & minor refactoring

--- get_zenodo_urls.py
+++ get_zenodo_urls.py
@@ -1,11 +1,31 @@
+
 """Write a file containing data file urls from zenodo for a given DOI
 example DOI is "10.5281/zenodo.16407"
 
 usage: python get_zenodo_url.py DOI output_filename
+
 """
 
 import sys
 import requests
+
+
+def get_dep_id(doi):
+	
+    """get the deposition id for the DOI"""
+	
+    response = requests.get(ZENODO_URL_BASE + "?access_token=" + ACCESS_TOKEN)	
+
+    json_data = response.json()
+	
+    for deposition in json_data:
+        if deposition["doi"] == str(doi):
+            dep_id = deposition["id"]	
+    return dep_id
+
+
+ZENODO_URL_BASE="https://zenodo.org/api/deposit/depositions/"
+
 
 #Zenodo Access token - use Phil's for demo
 #TODO: take as argument when available from web app user config
@@ -16,32 +36,18 @@ ACCESS_TOKEN="koumNv9K77DM2mehN1pEzOpuzjV89OhpHhSldUWx877Y6R5LniXMTA8iU1hD"
 doi = sys.argv[1]
 output_filename = sys.argv[2]
 
-def get_dep_id(doi):
-	
-	"""get the deposition id for the DOI"""
-	
-	response = requests.get("https://zenodo.org/api/deposit/depositions/?access_token="+ACCESS_TOKEN)	
-
-	json_data = response.json()
-	
-	for deposition in json_data:
-		if deposition["doi"]==str(doi):
-			dep_id=deposition["id"]	
-	return dep_id
-
-
+#Get data file meta data from Zenodo
 dep_id = get_dep_id(doi)
-
-url="https://zenodo.org/api/deposit/depositions/"+str(dep_id)+"?access_token=koumNv9K77DM2mehN1pEzOpuzjV89OhpHhSldUWx877Y6R5LniXMTA8iU1hD"
-
+url = ZENODO_URL_BASE + str(dep_id) + "?access_token=" + ACCESS_TOKEN
 deposition_json = requests.get(url).json()
-	
+
+#Write data file names and Zenodo urls to output file
 with open(output_filename, 'w')as f:
 
-	for dep_file in deposition_json["files"]:
-		file_id=dep_file["id"]
-		url="https://zenodo.org/api/deposit/depositions/"+str(dep_id)+"/files/"+ str(file_id) +"?access_token=koumNv9K77DM2mehN1pEzOpuzjV89OhpHhSldUWx877Y6R5LniXMTA8iU1hD"
-		file_json = requests.get(url).json()
-		file_url = "https://zenodo.org/record/"+str(dep_id)+"/files/"+str(file_json["filename"])
+    for dep_file in deposition_json["files"]:
+        file_id = dep_file["id"]
+        url = ZENODO_URL_BASE + str(dep_id)+"/files/" + str(file_id) + "?access_token=" + ACCESS_TOKEN
+        file_json = requests.get(url).json()
+        file_url = "https://zenodo.org/record/"+str(dep_id)+"/files/"+str(file_json["filename"])
 	
-		f.write(file_json["filename"]+"	"+file_url+"\n")
+        f.write(file_json["filename"] + "	" + file_url + "\n")
