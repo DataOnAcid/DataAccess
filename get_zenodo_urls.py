7acfb8a56b1f7944f76e3e0768b43beb79ab6709
alysbrett <alysbrett@users.noreply.github.com>
2015-03-27 14:37:41 +0000

    Take output filename as 2nd arg

--- get_zenodo_urls.py
+++ get_zenodo_urls.py
@@ -1,23 +1,27 @@
 """Write a file containing data file urls from zenodo for a given DOI
 example DOI is "10.5281/zenodo.16407"
 
-usage: python get_zenodo_url 10.5281/zenodo.16407
+usage: python get_zenodo_url.py DOI output_filename
 """
 
 import sys
 import requests
-import pprint 
 
+#Zenodo Access token - use Phil's for demo
+#TODO: take as argument when available from web app user config
+ACCESS_TOKEN="koumNv9K77DM2mehN1pEzOpuzjV89OhpHhSldUWx877Y6R5LniXMTA8iU1hD"
 
-#DOI from command line arg
+#DOI & output file from command line args
+#TODO error handling!
 doi = sys.argv[1]
+output_filename = sys.argv[2]
 
 def get_dep_id(doi):
 	
 	"""get the deposition id for the DOI"""
-
-	response = requests.get("https://zenodo.org/api/deposit/depositions/?access_token=koumNv9K77DM2mehN1pEzOpuzjV89OhpHhSldUWx877Y6R5LniXMTA8iU1hD")
 	
+	response = requests.get("https://zenodo.org/api/deposit/depositions/?access_token="+ACCESS_TOKEN)	
+
 	json_data = response.json()
 	
 	for deposition in json_data:
@@ -32,7 +36,7 @@ url="https://zenodo.org/api/deposit/depositions/"+str(dep_id)+"?access_token=kou
 
 deposition_json = requests.get(url).json()
 	
-with open('zonodo_files.txt', 'w')as f:
+with open(output_filename, 'w')as f:
 
 	for dep_file in deposition_json["files"]:
 		file_id=dep_file["id"]
